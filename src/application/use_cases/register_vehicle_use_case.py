71a839a26cc441bd82c12868b66e565de0e391b9
gerardoleyton <gerardleython@gmail.com>
2026-01-14 00:53:15 -0500

    fix: corregir formato de imports y longitud de línea para pasar Ruff en CI

--- src/application/use_cases/register_vehicle_use_case.py
+++ src/application/use_cases/register_vehicle_use_case.py
@@ -1,10 +1,10 @@
 """Use case for registering a new vehicle in the system."""
 
 from src.domain.entities.vehicle import Vehicle
-from src.domain.exceptions.duplicate_vehicle_exception import DuplicateVehicleException
-from src.domain.exceptions.vehicle_not_found_exception import (
-    VehicleNotFoundException,
+from src.domain.exceptions.duplicate_vehicle_exception import (
+    DuplicateVehicleException,
 )
+from src.domain.exceptions.vehicle_not_found_exception import VehicleNotFoundException
 from src.domain.ports.vehicle_repository import VehicleRepository
 
 
@@ -67,7 +67,7 @@ class RegisterVehicleUseCase:
                         ya_existe = any(
                             a.alert_type == alert_type and a.mileage == threshold
                             for a in existentes
-                        )
+                        )  # noqa: E501
                         if not ya_existe:
                             alert = MaintenanceAlert(
                                 id=f"A-{vehicle_id}-{threshold}-{alert_type.value}",
